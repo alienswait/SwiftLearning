9f5a5b53f2eb930467458c4ac0be5668d83d3fcb
Mert can Ünek <mrtcnunk@gmail.com>
2023-02-05 15:58:44 +0300

    first commit

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,29 @@
+import UIKit
+
+
+// Variables and Constants
+
+// String
+var userName = "Mert Can"
+userName.append("a")
+userName.lowercased()
+userName.uppercased()
+
+var userSurname = "Unek"
+
+// Integer && Double && Float
+
+//Integer
+let userAge = 50
+let myNumber = 4
+userAge / myNumber
+
+//Double
+let userAgeD = 50.0
+let myNumberD = 4.0
+userAgeD / myNumberD
+
+//Boolean
+
+var myBlooean = false
+myBlooean = true
