8ccad063b412fbe7ee82ad81fbe14b9ed5e4471c
Mert can Ünek <mrtcnunk@gmail.com>
2023-02-06 20:27:21 +0300

    .

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -42,3 +42,35 @@ let myUpperVariable = myVariable.uppercased()
 print (myUpperVariable)
 print(myVariable)
 
+// Arrays
+
+var myFavoriteMovies = ["Lord of the rings","Mad max","Constantine"]
+
+myFavoriteMovies[0].uppercased()
+myFavoriteMovies.count
+myFavoriteMovies[myFavoriteMovies.count - 1]
+myFavoriteMovies.last
+myFavoriteMovies.sort()
+
+// Set
+// Unordered collection, unique elements
+
+var mySet: Set = [1,2,3,4,5]
+var myStringSet: Set = ["a","b","c","a"]
+var myInternetArray = [1,2,3,4,5,1,2,3]
+var myInternetSet = Set (myInternetArray)
+print(myInternetArray)
+print(myInternetSet)
+
+var mySet1 : Set = [1,2,3]
+var mySet2: Set = [3,4,5]
+
+var mySet3 = mySet1.union(mySet2)
+print(mySet3)
+
+// Dictionary
+//key-value pairing
+
+var myFavoriteDirectors = ["Pulp Fiction" : "Tarantino","Lock, Stock" : "Guy Ritche", "The Dark Knight": "Cristoper Nolan"]
+myFavoriteDirectors["The Dark Knight"]
+myFavoriteDirectors["Lock, Stock"]
