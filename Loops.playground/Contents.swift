d436a57927bf0aabd09392479b12e98354325e4d
Mert can Ünek <mrtcnunk@gmail.com>
2023-02-06 20:56:14 +0300

    .

---
+++ Loops.playground/Contents.swift
@@ -0,0 +1,45 @@
+import UIKit
+
+var number  = 0
+
+// While Loop
+while number < 10 {
+    
+    //print(number)
+    number += 1
+    
+}
+
+var characterAlive = true
+while characterAlive == true {
+    
+    //print("character alive")
+    characterAlive = false
+}
+
+// For Loop
+
+var myFruitArray = ["Banana","Orange","Apple"]
+
+for i in myFruitArray {
+    
+    //print(i)
+}
+
+var myNumbers = [10,20,30,40,50]
+
+for i in myNumbers{
+    //print(i/5)
+}
+
+
+// If
+
+var myAge = 32
+if myAge < 30 {
+    print("30 yasindan kucuksun")
+}
+else{
+    print("30 yasindan buyuksun")
+}
+
